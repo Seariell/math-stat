cba153bacb749077e3795963cb87795b2dd731b3
Nataliya Odeeva <nataus@yandex.ru>
2020-09-11 00:42:19 +0300

    Исправление ошибкки

--- hw3/task_2.py
+++ hw3/task_2.py
@@ -16,19 +16,17 @@ pull_b1, pull_b2 = 2, 4
 
 # Два белых из первого и 1 белый из второго:
 p1 = combinations(pull_b1, box_1_white) / combinations(pull_b1, box_1) * \
-     combinations(1, box_2_white) / combinations(1, box_2) * \
-     combinations(3, box_2 - box_2_white) / combinations(3, box_2)
+     combinations(1, box_2_white) * combinations(3, box_2 - box_2_white) / combinations(pull_b2, box_2)
 print(f'Два белых из первого и 1 белый из второго = {p1*100:.2f}%')
+
 # Один белый из первого и два белых из второго:
-p2 = combinations(1, box_1_white) / combinations(1, box_1) * \
-     combinations(1, box_1 - box_1_white) / combinations(1, box_1) * \
-     combinations(2, box_2_white) / combinations(2, box_2) * \
-     combinations(2, box_2 - box_2_white) / combinations(2, box_2)
+p2 = combinations(1, box_1_white) * combinations(1, box_1 - box_1_white) / combinations(pull_b1, box_1) * \
+     combinations(2, box_2_white) * combinations(2, box_2 - box_2_white) / combinations(pull_b2, box_2)
 print(f'Один белый из первого и два белых из второго = {p2*100:.2f}%')
+
 # Три белых из второго:
-p3 = combinations(2, box_1 - box_1_white) / combinations(2, box_1) * \
-     combinations(3, box_2_white) / combinations(3, box_2) * \
-     combinations(1, box_2 - box_2_white) / combinations(1, box_2)
+p3 = combinations(2, box_1 - box_1_white) / combinations(pull_b1, box_1) * \
+     combinations(3, box_2_white) * combinations(1, box_2 - box_2_white) / combinations(pull_b2, box_2)
 print(f'Три белых из второго = {p3*100:.2f}%')
 p = p1 + p2 + p3
 print(f'Три белых = {p*100:.2f}%')
